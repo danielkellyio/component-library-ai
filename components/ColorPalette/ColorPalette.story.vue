f2d1357522a931b09d529924a09cb4ab47120549
Daniel Kelly <me@danielkelly.io>
2025-04-03 15:11:52 -0500

    wip

--- components/ColorPalette/ColorPalette.story.vue
+++ components/ColorPalette/ColorPalette.story.vue
@@ -7,13 +7,28 @@ const story = {
   title: "Components/ColorPalette",
   variants: [],
 } satisfies Story;
+
+const gradientColors = {
+  primary: ["primary-bg-gradient", "text-primary-content"],
+  secondary: ["secondary-bg-gradient", "text-secondary-content"],
+  neutral: ["neutral-bg-gradient", "text-neutral-content"],
+  info: ["info-bg-gradient", "text-info-content"],
+  success: ["success-bg-gradient", "text-success-content"],
+  warning: ["warning-bg-gradient", "text-warning-content"],
+  error: ["error-bg-gradient", "text-error-content"],
+};
 </script>
 
 <template>
-  <Story :title="story.title" :layout="{ type: 'grid', width: '100%' }">
+  <Story :title="story.title" :layout="{ type: 'grid', width: '50%' }">
     <!-- Default Variant -->
     <Variant title="Default">
       <ColorPalette />
+    </Variant>
+
+    <!-- Gradient Variant -->
+    <Variant title="Gradients">
+      <ColorPalette :colors="gradientColors" />
     </Variant>
   </Story>
 </template>
