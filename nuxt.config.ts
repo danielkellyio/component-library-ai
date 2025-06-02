180d7fc9807435ec9738d00493ab4789c2840c0e
Daniel Kelly <me@danielkelly.io>
2025-06-02 15:03:42 -0500

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/main.css'],
+  css: ["~/assets/main.css"],
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    'reka-ui',
-    '@vueuse/nuxt',
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "reka-ui",
+    "@vueuse/nuxt",
+    "@vueuse/motion/nuxt",
   ],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-})
+});
