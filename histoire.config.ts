3892f5f77b124c5e604e6baf29e78b0d96d5bf01
Daniel Kelly <me@danielkelly.io>
2025-06-02 16:04:38 -0500

    fix: tanstack import error

--- histoire.config.ts
+++ histoire.config.ts
@@ -5,4 +5,21 @@ import { HstNuxt } from "@histoire/plugin-nuxt";
 export default defineConfig({
   setupFile: "./histoire.setup.ts",
   plugins: [HstVue(), HstNuxt()],
+  vite: {
+    resolve: {
+      alias: {
+        "@tanstack/vue-virtual/dist/esm":
+          "@tanstack/vue-virtual/dist/esm/index.js",
+        "@tanstack/virtual-core/dist/esm":
+          "@tanstack/virtual-core/dist/esm/index.js",
+      },
+      dedupe: ["@tanstack/vue-virtual", "@tanstack/virtual-core"],
+    },
+    optimizeDeps: {
+      include: ["@tanstack/vue-virtual", "@tanstack/virtual-core"],
+    },
+    ssr: {
+      noExternal: ["@tanstack/vue-virtual", "@tanstack/virtual-core"],
+    },
+  },
 });
