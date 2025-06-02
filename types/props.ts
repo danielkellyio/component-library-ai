22bdf7dbb96f24156d49ea4fab347deaa930c623
Daniel Kelly <me@danielkelly.io>
2025-06-02 15:11:55 -0500

    wip

--- types/props.ts
+++ types/props.ts
@@ -1,15 +1,1 @@
 // This file contains the common props shared by some components
-
-export type size = "sm" | "md" | "lg";
-
-export type color =
-  | "default"
-  | "primary"
-  | "secondary"
-  | "neutral"
-  | "info"
-  | "success"
-  | "warning"
-  | "error";
-
-export type gradient = boolean;
